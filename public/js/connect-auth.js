e1f9b423f6c691b5fbebf4ea9d397a0cbcdbe72f
Sebastien PIC CS402 <killman@openwebtech.fr>
2012-04-12 17:15:56 +0200

    add validation on forms

--- public/js/connect-auth.js
+++ public/js/connect-auth.js
@@ -1,29 +1,70 @@
+//Set the validate parametters for the authForm and subForm
+$(document).ready(function(){
+	   //Set the rules on the subForm
+	   //thx to the jquery validate plugin : http://ajax.aspnetcdn.com/ajax/jquery.validate/1.9/jquery.validate.js
+	    $("#subForm").validate({
+			rules: {
+			// simple rule, converted to {required:true}
+			  "subLogin": {
+			      "required": true
+			  },
+			  "subPassword": {
+			      "required": true
+			  },
+			  "subMail":{
+			      "required": true,
+			      "email":true
+			  }
+			  
+			},
+			invalidHandler: function(){
+			  //things to be displayed in case of non valid subform
+			  //alert('invalid form');
+			}
+		});
+	    
+	    $("#login_form").validate({
+			rules: {
+				"nickInput": {
+					"required": true
+				},
+				"passwordInput":{
+					"required": true
+				}
+			}
+		
+		});
+	    
+});
 //check user's credential
 $("#connectButton").on("click",function(){
-	$.ajax({
-	  type: "GET",
-	  dataType: "json",
-	  url: "/checkuser?login=" + nickInput.value + "&password=" + passwordInput.value,
-	  error: function(data){
-		//$("div.auth-failed").css("display","block");
-		var res = JSON.stringify(data);
-		alert(res);
-	  },
-	  success: function(data){
-	    //  var parsedData = $.parseJSON(data);
-	    //Think about the transition's login
-	    // $('div.login_resp').html(data.check);
-	      if(data.check == 'ok'){
-		window.setTimeout('location.reload()', 1000);
-	      }else{
-		$('div.auth-failed').show();
-	      }
-	  }
-	  
-      });
+	if($("#login_form").valid()){
+		$.ajax({
+		  type: "GET",
+		  dataType: "json",
+		  url: "/checkuser?login=" + nickInput.value + "&password=" + passwordInput.value,
+		  error: function(data){
+			//$("div.auth-failed").css("display","block");
+			var res = JSON.stringify(data);
+			alert(res);
+		  },
+		  success: function(data){
+		    //  var parsedData = $.parseJSON(data);
+		    //Think about the transition's login
+		    // $('div.login_resp').html(data.check);
+		      if(data.check == 'ok'){
+			window.setTimeout('location.reload()', 1000);
+		      }else{
+			$('div.auth-failed').show();
+		      }
+		  }
+		  
+	      });
+	}
   });
 //-- Subscribe a new user
 $("#subButton").on("click",function(){
+	if($("#subForm").valid()){	
 		$.ajax({
 			type: "GET",
 			url: "/adduser",
@@ -40,6 +81,7 @@ $("#subButton").on("click",function(){
                                 }
 			}
 		});
+	}	
 		
 		
 	});
